67a96a1041a3692fa8c1d25dbf8f3363b930a52b
Luiz Wynne <luiz@fintechjobs.ie>
2021-01-10 19:36:10 -0300

    Small adjusts FE

--- src/components/projetos/Projeto.js
+++ src/components/projetos/Projeto.js
@@ -1,9 +1,10 @@
 import React, { Fragment, useEffect } from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getProjectById } from '../../actions/projeto'
 import Spinner from '../layouts/Spinner';
 import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
 
 const Projeto = ({ getProjectById, auth, 
     projeto:{
@@ -26,7 +27,20 @@ const Projeto = ({ getProjectById, auth,
                     <Spinner/>
                 :
                     <Fragment>
-                        { projeto.nome }
+                        <h3 class="text-dark">{projeto.nome}</h3>
+                        <p>
+                            
+                        </p>
+                        <p><strong>Protocolo: </strong>{projeto.protocolo}</p>
+                        <p><strong>Tipo: </strong>{projeto.tipo.nome}</p>
+                        <p><strong>Data: </strong><Moment format='YYYY/MM/DD'>{projeto.created_at}</Moment></p>
+                        <br />
+                        <p><strong>Descricao: </strong><br />{projeto.descricao}</p>
+                        <br />
+                        <p><strong>Status: </strong>{ projeto.aprovado === null ? 'Pendente' : 'Finalizado' }</p>
+                        <p><strong>Aprovado: </strong>{ projeto.aprovado === null ? 'Projeto em votação' : (projeto.aprovado ? 'Sim' : 'Não') }</p>
+                        <p><strong>Total de votos: </strong>{projeto.total_votos}</p>
+                        <p><strong>Relator: </strong>{projeto.relator.nome}</p>
                     </Fragment>
 
 
